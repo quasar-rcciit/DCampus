8a9ea8b77f45667281f86fd53ec9db44332679fc
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2021-09-23 00:24:40 +0530

    added cards and Navbar update

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="card-link">
+            Card link
+          </a>
+          <a href="#" class="card-link">
+            Another link
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+export default Card;
